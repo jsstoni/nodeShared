2fb8a123aacaa392128f97e017733a4cb0329eb4
jsstoni <counsantor2d@gmail.com>
2015-12-09 14:00:15 -0400

    creando script ajax

---
+++ manager/panel/client/JS/script.js
@@ -0,0 +1,15 @@
+$(function() {
+  $("#login").on("submit", function(event) {
+    var url = $(this).attr("action");
+    var data = $(this).serialize();
+    $.ajax({
+      type : "POST",
+      url: url,
+      data : data,
+      dataType: 'jsonp',
+      success:function(data) {
+        console.log(data);
+      }
+    });
+  });
+}); 
--- manager/panel/index.php
+++ manager/panel/index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>NODE ADMIN</title>
-	<script src="../client/JS/jquery.js"></script>
+  <title>NODE ADMIN</title>
+  <script src="../client/JS/jquery.js"></script>
 </head>
 <body>
-<form action="../index.php?daemon=admin&exec=start" method="post">
-	<input type="password" name="pass">
-</form>
+  <form action="../index.php" method="post" id="login">
+    <input type="text" name="daemon">
+    <select name="exec">
+      <option value="start">START</option>
+      <option value="stop">STOP</option>
+    </select>
+    <input type="password" name="pass">
+    <button type="submit">NODE</button>
+  </form>
 </body>
 </html>
